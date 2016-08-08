663f70b9f9c1d86760a37a0678023285cedde338
Pete Deevakul <editor@mozzarella.website>
2016-08-08 19:35:53 -0400

    rvest to extract data from html - single pages

---
+++ R/scrape-cleanse-02.R
@@ -0,0 +1,87 @@
+# scrape-clean v1
+# getting the information out
+# with rvest
+
+library(rvest)
+library(tm)
+
+# p directory - single page ---------------------------------------------------
+
+# set path to scraped files
+datDir02 <- "~/GitHub/agora/2014-01-01/p/a37plcuKol"
+
+# test
+agora02 <- read_html(datDir02)
+
+# extract product name/listing headline -------------------
+title02 <- agora02 %>%
+  html_nodes("title") %>%
+  html_text()
+
+title02
+# [1] "10 regular seeds - Skunk Kush - pain relief"
+
+
+# extract description -------------------------------------
+
+# selector for html_nodes() is not specific enough. 
+# it's possible to harvest the whole listing 
+# then separate out with regex later.
+# but there are no css selectors for the text description
+# might be an xml tag. 
+
+description02 <- agora02 %>%
+  html_nodes("#single-product") %>%
+  html_text()
+
+description
+
+# extract price -------------------------------------------
+price02 <- agora02 %>%
+  html_nodes(".product-page-price") %>%
+  html_text()
+
+price02
+# [1] "0.05353963 BTC"
+
+# vendor --------------------------------------------------
+vendor02 <- agora02 %>%
+  html_nodes("a.gen-user-link ") %>%
+  html_attr("href")
+
+vendor02
+# [1] "/vendor/budbrother"
+
+# remove the dir path in front of vendor name
+vendor02 <- gsub("/vendor/", "", vendor02)
+vendor02
+# [1] "budbrother"
+
+# ratings -------------------------------------------------
+
+rating02 <- agora02 %>%
+  html_nodes(".gen-user-ratings") %>%
+  html_text()
+
+rating02
+# [1] " [0 deals]"
+
+# ships from ----------------------------------------------
+
+ship_from02 <- agora02 %>%
+  html_nodes(".product-page-ships") %>%
+  html_text()
+
+ship_from02
+# [1] "\r\n      Ships from  Australia\r\n    \r\n    "
+
+# clean up info
+ship_from02 <- gsub("Ships from", "", ship_from02)
+ship_from02 <- stripWhitespace(ship_from02)
+ship_from02
+# [1] " Australia "
+
+# data frame ------------------------------------------------------------------
+
+agora <- data.frame(title = title, price = price, description = description, 
+                    vendor = vendor, rating = rating, ship_from = ship_from)
---
+++ R/scrape-cleanse.R
@@ -0,0 +1,79 @@
+# scrape-clean v1
+# getting the information out
+# with rvest
+
+library("rvest")
+
+# p directory - single page ---------------------------------------------------
+
+# set path to scraped files
+datDir <- "~/GitHub/agora/2014-01-01/p/a1BkWEipyk"
+
+# test
+agora01 <- read_html(datDir)
+
+# extract product name/listing headline -------------------
+title <- agora01 %>%
+  html_nodes("title") %>%
+  html_text()
+
+title
+# [1] "Secret of Methamphetamine Manufacture 8th Edition"
+
+
+# extract description -------------------------------------
+
+# selector for html_nodes() is not specific enough. 
+# it's possible to harvest the whole listing 
+# then separate out with regex later.
+# but there are no css selectors for the text description
+# might be an xml tag. 
+
+description <- agora01 %>%
+  html_nodes("#single-product") %>%
+  html_text()
+
+description
+
+# extract price -------------------------------------------
+price <- agora01 %>%
+  html_nodes(".product-page-price") %>%
+  html_text()
+
+price
+# [1] "0.00266844 BTC"
+
+# vendor --------------------------------------------------
+vendor <- agora01 %>%
+  html_nodes("a.gen-user-link ") %>%
+  html_attr("href")
+
+vendor
+# [1] "/vendor/passman"
+
+# remove the dir path in front of vendor name
+vendor <- gsub("/vendor/", "", vendor)
+vendor
+
+# ratings -------------------------------------------------
+
+rating <- agora01 %>%
+  html_nodes(".gen-user-ratings") %>%
+  html_text()
+ 
+rating
+# [1] "  5.0/5, 15~25 deals"
+
+# ships from ----------------------------------------------
+
+ship_from <- agora01 %>%
+  html_nodes(".product-page-ships") %>%
+  html_text()
+
+ship_from
+
+
+# data frame ----------------------------------------------
+
+agora <- data.frame(title = title, price = price, description = description, 
+                    vendor = vendor, rating = rating)
