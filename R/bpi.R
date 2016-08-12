55e1c279618b90e0bca65bd917f93e2df88f9f98
Pete Deevakul <editor@mozzarella.website>
2016-08-12 13:42:44 -0400

    general script reading in bitcoin price index data

---
+++ R/bpi.R
@@ -0,0 +1,17 @@
+# Agora Marketplace Analysis
+# bitcoin price index data
+
+
+# via coindesk.com
+bpi_coin <- read.csv("data/bpi-Coindesk.csv")
+bpi_coin$Date <- as.Date(bpi_coin$Date)
+
+bpiAg <- subset(bpi_coin, bpi_coin$Date > "2013-12-31")
+bpiAg$btcAge <- rownames(bpiAg)
+rownames(bpiAg) <- NULL
+write.table(bpiAg, file = "data/bpi-Agora.csv", sep = ",", row.names = F)
+
+# via blockchain.info
+bpi_block <- read.csv("data/bpi-blockchain.csv")
+colnames(bpi_block) <- c("date", "price")
+bpi_block$date <- as.Date(bpi_block$date)
