01b9c181ecd44936f816bcd6e2f8e92472ffac65
Pete Deevakul <editor@mozzarella.website>
2016-08-17 03:27:04 -0400

    exploratory plots of categoricals

--- R/p-0114-sample.R
+++ R/p-0114-sample.R
@@ -40,16 +40,169 @@ levels(fb$subsubcat)
 library(ggplot2)
 library(RColorBrewer)
 library(vcd)
+library(extrafont)
+library(colorRamps)
+library(grDevices)
 
 display.brewer.all()
-ppal <- c("")
+font_import()
+fonts()
 
+# color
+rich12equal = c("#000040", "#000093", "#0020E9", "#0076FF", 
+                "#00B8C2", "#04E466", "#49FB25", "#E7FD09", 
+                "#FEEA02", "#FFC200", "#FF8500", "#FF3300")
+
+rich10equal = c("#000041", "#0000A9", "#0049FF", "#00A4DE", 
+                "#03E070", "#2fa502", "#F6F905", "#FFD701", 
+                "#FF9500", "#FF3300")
+
+heat <- heat.colors(32)
+
+# categorical counts
 cat <- ggplot(p14, aes(x = cat, fill = cat)) + 
-  geom_bar(stat = "count", color = "black") +
-  scale_fill_manual(values = c("#EED5B7", "#8B1A1A"))
+  geom_bar(stat = "count", color = "black", alpha = 0.75) 
 
-cat <- cat + theme_minimal() +
-  theme(plot.margin = unit(c(2, 2, 2, 2), "cm"))
+cat <- cat + 
+  scale_fill_manual(values = rich10equal) +
+  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
+  theme(plot.margin = unit(c(2, 2, 0.5, 2), "cm")) +
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1.25,
+                                   size = 14, face = "bold")) +
+  theme(plot.title = element_text(size = 18, face = "bold")) +
+  labs(title = "Agora Marketplace: Main Categories",
+       x = "", y  = "count")
 
-cat 
+cat
 
+# subcategory counts
+length(levels(p14$subcat)) # 32
+
+subcat <- ggplot(p14, aes(x = subcat, fill = subcat)) +
+  geom_bar(stat = "count", color = "black", alpha = 0.75) +
+  theme_minimal(base_size = 12, base_family = "FranklinGothicSSK") +
+  theme(plot.margin = unit(c(2, 2, 0.5, 2), "cm"))
+
+subcat <- subcat +
+  scale_fill_manual(values = heat) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1.25,
+                                   size = 14, face = "bold")) +
+  theme(axis.text.y = element_text(size = 14, face = "bold")) +
+  theme(plot.title = element_text(size = 18, face = "bold")) +
+  theme(legend.position = "none") +
+  labs(title = "Agora Marketplace: subcategories",
+       x = "", y  = "count")
+  
+subcat
+# revise NAs in subcategory - fill in with category value
+
+# subsubcategory counts
+length(levels(p14$subsubcat)) # 25
+
+terr25 <- terrain.colors(25)
+
+subsub <- ggplot(na.omit(p14), aes(x = subsubcat, fill = subsubcat)) +
+  geom_bar(stat = "count", color = "black", fill = 0.75) +
+  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
+  theme(plot.margin = unit(c(2, 2, 0.5, 2), "cm"))
+
+subsub + theme(axis.text.x = element_text(angle = 45, hjust = 1.1, vjust = 1.25,
+                                   size = 14, face = "bold")) +
+  theme(axis.text.y = element_text(size = 14, face = "bold")) +
+  theme(plot.title = element_text(size = 18, face = "bold")) +
+  theme(legend.position = "none") +
+  labs(title = "Agora Marketplace: sub-subcategories",
+       x = "", y  = "count")
+
+subsub
+
+
+# feedback/transaction category counts
+
+fbCat <- ggplot(fb, aes(x = cat, fill = cat)) + 
+  geom_bar(stat = "count", color = "black", alpha = 1) 
+
+fbCat <- fbCat + 
+  scale_fill_brewer(palette = "Reds", direction = -1) +
+  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
+  theme(plot.margin = unit(c(2, 2, 0.5, 2), "cm")) +
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1.25,
+                                   size = 14, face = "bold")) +
+  theme(plot.title = element_text(size = 18, face = "bold")) +
+  labs(title = "Agora Marketplace: categories with feedback/transactions ",
+       x = "", y  = "count")
+
+fbCat
+
+# feedback/transaction subcat counts
+fb.subcat <- ggplot(fb, aes(x = subcat, fill = subcat)) +
+  geom_bar(stat = "count", color = "black", alpha = 1) +
+  theme_minimal(base_size = 12, base_family = "FranklinGothicSSK") +
+  theme(plot.margin = unit(c(2, 2, 0.5, 2), "cm"))
+
+fb.subcat <- fb.subcat +
+  scale_fill_manual(values = heat) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1.25,
+                                   size = 14, face = "bold")) +
+  theme(axis.text.y = element_text(size = 14, face = "bold")) +
+  theme(plot.title = element_text(size = 18, face = "bold")) +
+  theme(legend.position = "none") +
+  labs(title = "Agora Marketplace: subcategories with feedback/transactions",
+       x = "", y  = "count")
+
+fb.subcat
+
+# feedback/transactions sub-subcategory counts
+fb.subsub <- ggplot(na.omit(p14), aes(x = subsubcat, fill = subsubcat)) +
+  geom_bar(stat = "count", color = "black", fill = 0.75) +
+  theme_minimal(base_size = 14, base_family = "FranklinGothicSSK") +
+  theme(plot.margin = unit(c(2, 2, 0.5, 2), "cm"))
+
+fb.subsub + theme(axis.text.x = element_text(angle = 45, hjust = 1.1, vjust = 1.25,
+                                          size = 14, face = "bold")) +
+  theme(axis.text.y = element_text(size = 14, face = "bold")) +
+  theme(plot.title = element_text(size = 18, face = "bold")) +
+  theme(legend.position = "none") +
+  labs(title = "Agora Marketplace: sub-subcategories with feedback/transactions",
+       x = "", y  = "count")
+
+
+# cat by subcat
+par(mar = c(12, 12, 8, 8), family = "FranklinGothicSSK", las = 2)
+plot(p14$cat, p14$subcat, xlab = "", ylab = "", 
+     main = "AgMarketplace: categories by subcategories")
+
+#main
+par(mar = c(12, 12, 12, 8), las = 1)
+plot(p14$cat, main = "AgMarketplace: listing categories", horiz = TRUE)
+
+# sub
+par(mar = c(12, 12, 12, 8), las = 1)
+plot(p14$subcat, main = "AgMarketplace: listing subcategories", horiz = TRUE)
+
+# subsub
+par(mar = c(12, 12, 12, 8), las = 1)
+plot(p14$subsubcat, main = "AgMarketplace: listing sub-subcategories", horiz = TRUE)
+
+# categories by location
+
+p14$from <- stripWhitespace(as.character(p14$from))
+levels(as.factor(p14$from))
+p14$from <- as.factor(p14$from)
+
+# subcat by ship_from
+par(mar = c(12, 18, 8, 8), family = "FranklinGothicSSK", las = 2)
+plot(p14$subcat, p14$from, main = "AgMarket: subcategory by location",
+     xlab = "", ylab = "")
+
+# cat by ship_from
+par(mar = c(12, 18, 8, 8), family = "FranklinGothicSSK", las = 2)
+plot(p14$cat, p14$from, main = "AgMarket: listing category by location",
+     xlab = "", ylab = "")
+
+# subsubcat by location
+par(mar = c(12, 18, 8, 8), family = "FranklinGothicSSK", las = 2)
+plot(p14$subsubcat, p14$from, main = "AgMarket: sub-subcategory by location",
+     xlab = "", ylab = "")
