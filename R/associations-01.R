540c050bf9b981366b7c785b4b1be7b87932198c
Pete Deevakul <editor@mozzarella.website>
2016-08-24 18:51:36 -0400

    Poisson model on product counts by date
    residual deviance 4000x degrees of freedom.

--- R/associations-01.R
+++ R/associations-01.R
@@ -173,6 +173,7 @@ p$date <- as.Date(p$date)
 p$vendor <- as.integer(p$vendor)
 
 # linear model 01 -------------------------------------------------------------
+
 pd.lm01 <- ggplot(p, aes(date, p)) + 
   theme_minimal() +
   geom_point(aes(color = p), size = 4.75, shape = 17) +
@@ -190,6 +191,39 @@ pd.lm01 + stat_smooth(method = lm, level = 0.95, se = FALSE, colour = "#CD2626",
   theme(axis.title.y = element_text(margin = margin(0, 20, 0, 0))) + 
   theme(axis.title.x = element_text(margin = margin(40, 0, 0, 0)))
 
+# Poisson model on Product Counts by date -------------------------------------
+
+pd.glm01 <- glm(p ~ date, family = "poisson", data = p)
+summary(pd.glm01)
+# Deviance Residuals: 
+#      Min        1Q    Median        3Q       Max  
+# -187.420   -59.783    -0.658    53.341    93.563 
+
+#                     Estimate Std. Error z value Pr(>|z|)    
+#  (Intercept)     -3.319e+01  8.372e-02  -396.4   <2e-16 ***
+#  date             2.593e-03  5.090e-06   509.5   <2e-16 ***
+
+#     Null deviance: 1214565  on 202  degrees of freedom
+# Residual deviance:  932944  on 201  degrees of freedom
+# AIC: 935136
+
+# That Z-Value is through the roof. 
+
+options(scipen = 4)
+pd.glm02 <- glm(p ~ date + vendor, family = "poisson", data = p)
+summary(pd.glm02)
+# Deviance Residuals: 
+#      Min        1Q    Median        3Q       Max  
+# -223.648   -38.573    -1.081    30.056   143.665 
+
+#                     Estimate Std. Error    z value Pr(>|z|)    
+#   (Intercept) -28.620540551   0.087994160  -325.3   <2e-16 ***
+#   date          0.002283534   0.000005363   425.8   <2e-16 ***
+#   vendor        0.005016896   0.000012577   398.9   <2e-16 ***
+
+#     Null deviance: 1214565  on 202  degrees of freedom
+# Residual deviance:  768506  on 200  degrees of freedom
+# AIC: 770700
 
 
 
