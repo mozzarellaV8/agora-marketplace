cb11a40f51892104eb1b9a6f38c6b8d5e0c96021
Pete Deevakul <editor@mozzarella.website>
2016-08-26 14:45:14 -0400

    inspect rules for all sub-subcategories

--- R/association-rules-2014.R
+++ R/association-rules-2014.R
@@ -185,6 +185,7 @@ v2rules
 # set of 3492 rules 
 summary(v2rules)
 
+# try out various subsets
 cannabis <- subset(v2rules, subset = rhs %in% "cat=Drugs" & lift > 1.2) # 709 rules
 synthetics <- subset(v2rules, subset = rhs %in% "subcat=Cannabis" & lift > 1.2)
 # 230 rules
@@ -196,4 +197,261 @@ arules::inspect(head(cannabis))
 arules::inspect(head(synthetics))
 arules::inspect(tail(synthetics))
 arules::inspect(tail(cannabis))
+
+# the Question of Distribution ------------------------------------------------
+
+# this is somewhat like building a profile - subsetting for presumed 
+# attributes of someone with intent to distribute rather than 
+# consume personally.
+
+# highlight features of interest - wide range
+levels(fb$cat)
+# "Counterfeits" - identities
+# "Drug paraphernalia"
+# "Drugs"
+# "Forgeries" - identities
+# "Information" - how to / guides
+# "Electronics" - check for scales
+# "Weapons"
+
+levels(fb$subcat)
+# "Accessories"
+# "Advertising" 
+# "Ammunition"
+# "Cannabis"
+# "Electronics"
+# "Grinders"
+# "Guides"
+# "Other
+# "Paper"
+# "Lethal firearms"
+# "Melee"
+# "Money"
+# "Non-lethal firearms"
+# "Other
+# "Paper"
+# "Paraphernalia"
+# "Physical documents"
+# "Pipes"
+# "Prescription"
+# "Scales"
+# "Scans/Photos"
+# "Smoked"
+# "Stashes"
+# "eBooks"
+
+levels(fb$subsubcat)
+# "Concentrates"
+# "Edibles"
+# "Hash"
+# "Other"
+# "Others"
+# "Prescription"
+# "Seeds"
+# "Shake/trim"
+# "Synthetics"
+# "Weed"
+
+# marijuana-related Rule Subsets ----------------------------------------------
+
+# Weed ----------------------------------------------------
+mj <- subset(v2rules, subset = rhs %in% "subsubcat=Weed" & lift > 1.2) # 98 rules
+summary(mj)
+# set of 98 rules
+
+# rule length distribution (lhs + rhs):sizes
+# 2  3  4  5 
+# 12 37 37 12 
+
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 2.0     3.0     3.5     3.5     4.0     5.0 
+
+# summary of quality measures:
+#          support       confidence            lift      
+# Min.   :0.001408   Min.   :0.6025   Min.   :4.252  
+# 1st Qu.:0.001968   1st Qu.:0.7088   1st Qu.:5.002  
+# Median :0.002294   Median :0.9529   Median :6.724  
+# Mean   :0.008015   Mean   :0.8513   Mean   :6.007  
+# 3rd Qu.:0.003104   3rd Qu.:0.9663   3rd Qu.:6.819  
+# Max.   :0.141716   Max.   :1.0000   Max.   :7.056  
+
+# mining info:
+#   data ntransactions support confidence
+#     v2        349545  0.0014        0.6
+
+arules::inspect(head(mj))
+arules::inspect(tail(mj))
+
+# Synthetics ----------------------------------------------
+synth <- subset(v2rules, subset = rhs %in% "subsubcat=Synthetics")
+synth
+# set of 0 rules
+
+synthCheck <- subset(fb, (fb$subsubcat == "Synthetics"))
+# 1356 obs.
+
+levels(fb$price)
+levels(synthCheck$price)
+
+table(synthCheck$price)
+# [1.00e-07,1.49e+00) [1.07e+01,2.00e+01) [1.19e+02,2.22e+02) [1.49e+00,4.85e+00) [2.00e+01,3.56e+01) [2.09e+03,3.20e+03] [2.22e+02,4.42e+02) [3.56e+01,6.37e+01) 
+# 1273                   0                   0                  62                   0                   0                   0                   0 
+# [4.42e+02,9.48e+02) [4.85e+00,1.07e+01) [6.37e+01,1.19e+02) [9.48e+02,2.09e+03) 
+# 0                  21                   0                   0 
+
+table(fb$price)
+# [1.00e-07,1.49e+00) [1.07e+01,2.00e+01) [1.19e+02,2.22e+02) [1.49e+00,4.85e+00) [2.00e+01,3.56e+01) [2.09e+03,3.20e+03] [2.22e+02,4.42e+02) [3.56e+01,6.37e+01) 
+# 316365                1487                  79               24034                 644                  21                 118                 130 
+# [4.42e+02,9.48e+02) [4.85e+00,1.07e+01) [6.37e+01,1.19e+02) [9.48e+02,2.09e+03) 
+# 56                6457                  95                  59
+
+
+unique(synthCheck$vendor)
+#  [1] SusnaSmith           Heisenbergmontana    Charlie_Bartlett     axa                 
+# [5] Illegal_Entrepreneur DoctorNick           nawlins              b1g1mpact           
+# [9] CarlBildt            EZTest               aussieherbalsmoke    FerrariBlack1       
+#  [13] lamachine            MrSunshine           ImportDirect         USAXpress           
+# [17] drzheng              SatoshiShop          alchemycd            FREE                
+# [21] TheAvatarSpirit
+
+unique(synthCheck$from)
+unique(synthCheck$product)
+
+# Shake/trim ----------------------------------------------
+
+shake <- subset(v2rules, rhs %in% "subsubcat=Shake/trim")
+shake
+# set of 0 rules
+
+shakeCheck <- subset(fb, fb$subsubcat == "Shake/trim")
+# 244 obs.
+
+unique(shakeCheck$vendor)
+# [1] GucciBuds   DrEarnhardt drugsRus    GrowerLove  
+#     GreenStreet ModernLove  DankBoss101 krautling
+
+unique(shakeCheck$from)
+# [1]  USA           Netherlands   UK           <NA>           Germany
+
+# Seeds ---------------------------------------------------
+
+seeds <- subset(v2rules, rhs %in% "subsubcat=Seeds")
+seeds
+# set of 0 rules
+
+seedCheck <- subset(fb, fb$subsubcat == "Seeds")
+# 1298 variables
+unique(seedCheck$vendor)
+#  [1] simonlabond          Cannaseed            budbrother           toysoldiers         
+# [5] PeterLustig          FreshTouch           HappyHippy2.0        superstrains        
+# [9] silverapples         Gobotal              Seeds_Co             Spacelab            
+# [13] Renegade             Quantum.Black.Widow  My_Private_Garden    Charlie_Bartlett    
+# [17] derriese5            StrainHunters        Warbeast             CaliforniaGreenCross
+# [21] HangAbout            AngelEyes            klosterbier          danman              
+# [25] Alexandrus           OzGrow               BenzoAU
+
+# Prescription --------------------------------------------
+
+scrip <- subset(v2rules, rhs %in% "subsubcat=Prescription")
+scrip # set of 0 rules
+
+scripCheck <- subset(fb, fb$subsubcat == "Prescription")
+# 4932 observations - all stimulants
+unique(scripCheck$product)
+# 473 versions of Adderall, Dexedrine, Vyvanse, Ritalin, Modafanil, Provigil
+# Occasional Viagra and Cialis. 
+# A misplaced Codeine, Lorazepam/Ativan.
+# Could regex out some details later.
+
+# "Hash" --------------------------------------------------
+
+hash <- subset(v2rules, rhs %in% "subsubcat=Hash")
+hash # set of 8 rules
+summary(hash)
+
+arules::inspect(head(hash, n = 3, by = "confidence"))
+arules::inspect(tail(hash, n = 3, by = "confidence"))
+
+arules::inspect(head(hash, n = 3, by = "confidence"))
+arules::inspect(tail(hash, n = 3, by = "confidence"))
+
+arules::inspect(head(hash, n = 5, by = "support"))
+arules::inspect(tail(hash, n = 5, by = "support"))
+arules::inspect(tail(hash, n = 5, by = "lift"))
+arules::inspect(tail(hash, n = 5, by = "lift"))
+
+# "Edibles" -----------------------------------------------
+
+ed <- subset(v2rules, rhs %in% "subsubcat=Edibles")
+summary(ed)
+# set of 8 rules
+
+# rule length distribution (lhs + rhs):sizes
+# 2 3 4 5 
+# 1 3 3 1 
+
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 2.0     3.0     3.5     3.5     4.0     5.0 
+
+# summary of quality measures:
+#   support           confidence          lift      
+# Min.   :0.002558   Min.   :0.6622   Min.   :47.04  
+# 1st Qu.:0.002558   1st Qu.:0.6683   1st Qu.:47.47  
+# Median :0.002683   Median :0.6900   Median :49.01  
+# Mean   :0.002683   Mean   :0.6899   Mean   :49.01  
+# 3rd Qu.:0.002809   3rd Qu.:0.7102   3rd Qu.:50.45  
+# Max.   :0.002809   Max.   :0.7173   Max.   :50.95  
+
+# mining info:
+#   data ntransactions support confidence
+# v2        349545  0.0014        0.6
+
+arules::inspect(ed)
+#      lhs                                                rhs                 support     confidence lift    
+# 412  {vendor=Lion}                                   => {subsubcat=Edibles} 0.002809366 0.6657627  47.28999
+# 1502 {vendor=Lion,subcat=Cannabis}                   => {subsubcat=Edibles} 0.002809366 0.7173119  50.95159
+# 1504 {vendor=Lion,cat=Drugs}                         => {subsubcat=Edibles} 0.002809366 0.6721424  47.74314
+# 1506 {vendor=Lion,greatFB}                           => {subsubcat=Edibles} 0.002557611 0.6622222  47.03850
+# 2709 {vendor=Lion,cat=Drugs,subcat=Cannabis}         => {subsubcat=Edibles} 0.002809366 0.7173119  50.95159
+# 2712 {vendor=Lion,subcat=Cannabis,greatFB}           => {subsubcat=Edibles} 0.002557611 0.7078385  50.27868
+# 2715 {vendor=Lion,cat=Drugs,greatFB}                 => {subsubcat=Edibles} 0.002557611 0.6691617  47.53142
+# 3361 {vendor=Lion,cat=Drugs,subcat=Cannabis,greatFB} => {subsubcat=Edibles} 0.002557611 0.7078385  50.27868
+
+# "Concentrates" ------------------------------------------
+
+concentrates <- subset(v2rules, rhs %in% "subsubcat=Concentrates")
+summary(concentrates)
+# set of 8 rules
+
+# rule length distribution (lhs + rhs):sizes
+# 2 3 4 5 
+# 1 3 3 1 
+
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 2.0     3.0     3.5     3.5     4.0     5.0 
+
+# summary of quality measures:
+#   support           confidence      lift      
+# Min.   :0.002323   Min.   :1    Min.   :42.36  
+# 1st Qu.:0.002323   1st Qu.:1    1st Qu.:42.36  
+# Median :0.002336   Median :1    Median :42.36  
+# Mean   :0.002336   Mean   :1    Mean   :42.36  
+# 3rd Qu.:0.002349   3rd Qu.:1    3rd Qu.:42.36  
+# Max.   :0.002349   Max.   :1    Max.   :42.36  
+
+# mining info:
+#   data ntransactions support confidence
+#     v2        349545  0.0014        0.6
+
+arules::inspect(concentrates)
+#      lhs                                                     rhs                      support     confidence lift    
+# 246  {vendor=chipzahoy}                                   => {subsubcat=Concentrates} 0.002348768 1          42.35882
+# 1093 {vendor=chipzahoy,subcat=Cannabis}                   => {subsubcat=Concentrates} 0.002348768 1          42.35882
+# 1095 {vendor=chipzahoy,cat=Drugs}                         => {subsubcat=Concentrates} 0.002348768 1          42.35882
+# 1097 {vendor=chipzahoy,greatFB}                           => {subsubcat=Concentrates} 0.002323020 1          42.35882
+# 2380 {vendor=chipzahoy,cat=Drugs,subcat=Cannabis}         => {subsubcat=Concentrates} 0.002348768 1          42.35882
+# 2383 {vendor=chipzahoy,subcat=Cannabis,greatFB}           => {subsubcat=Concentrates} 0.002323020 1          42.35882
+# 2386 {vendor=chipzahoy,cat=Drugs,greatFB}                 => {subsubcat=Concentrates} 0.002323020 1          42.35882
+# 3271 {vendor=chipzahoy,cat=Drugs,subcat=Cannabis,greatFB} => {subsubcat=Concentrates} 0.002323020 1          42.35882
+
 
