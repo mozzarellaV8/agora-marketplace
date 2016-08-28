adbea4bb29e57157e39962df557a6572b8a403c4
Pete Deevakul <editor@mozzarella.website>
2016-08-27 21:55:32 -0400

    identify price trend intervals

--- R/EDA-02.R
+++ R/EDA-02.R
@@ -16,7 +16,7 @@ levels(as.factor(fb$to))   #99
 
 # bitcoin price data
 bpi <- read.csv("data/bpi/bpi-Coindesk.csv")
-bpin$Date <- as.Date(bpi$Date)
+bpi$Date <- as.Date(bpi$Date)
 summary(bpi)
 
 bpi <- na.omit(bpi)
@@ -70,11 +70,14 @@ bpiAg[bpiAg$Price == mean(bpiAg$Price), ]
 mean(bpiAg$Price)
 # [1] 526.9241
 
-# subset Agora up/down trend intervals
+# subset Agora up/down trend intervals ----------------------------------------
+
+# 1st event - 1st downturn ----------------------------------------------------
+
 bpiAg04 <- bpiAg[bpiAg$Date >= "2014-04-01" & bpiAg$Date <= "2014-04-30", ]
 summary(bpiAg04$Price)
 #    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#  360.8   441.2   456.9   460.2   486.3   529.2
+#   360.8   441.2   456.9   460.2   486.3   529.2
 
 bpiAg04[bpiAg04$Price == 360.84, ]
 #            Date  Price
@@ -85,13 +88,15 @@ dim(dt01)
 # 100   2
 # 100 day downward trend in pricing.
 
+# start date
 dt01[1, ]
 #            Date  Price
 # 1264 2014-01-01 770.44
+
+# end date
 dt01[100, ]
 #            Date  Price
 # 1363 2014-04-10 360.84
-
 
 summary(dt01)
 #       Date                Price      
@@ -101,6 +106,41 @@ summary(dt01)
 # Mean   :2014-02-19   Mean   :678.8  
 # 3rd Qu.:2014-03-16   3rd Qu.:842.4  
 # Max.   :2014-04-10   Max.   :951.4
+
+# 2nd event: 1st Rally --------------------------------------------------------
+
+# identity peak value from range
+bpiAgSummer <- bpiAg[bpiAg$Date >= "2014-04-11" & bpiAg$Price >= 600, ]
+summary(bpiAgSummer)
+#          Date                Price      
+# Min.   :2014-05-30   Min.   :600.0  
+# 1st Qu.:2014-06-09   1st Qu.:618.8  
+# Median :2014-07-04   Median :627.6  
+# Mean   :2014-06-28   Mean   :629.2  
+# 3rd Qu.:2014-07-14   3rd Qu.:639.4  
+# Max.   :2014-07-24   Max.   :665.7
+
+bpiAgSummer[bpiAgSummer$Price == 665.73, ]
+#            Date  Price
+# 1417 2014-06-03 665.73
+
+nrow(bpiAgSummer)
+# [1] 41
+
+# 3rd event - downturn 2 ------------------------------------------------------
+
+dt02 <- bpiAg[bpiAg$Date >="2014-06-04" & bpiAg$Date <= "2014-12-01", ] 
+summary(dt02)
+# Date                Price      
+# Min.   :2014-06-04   Min.   :319.6  
+# 1st Qu.:2014-07-19   1st Qu.:377.1  
+# Median :2014-09-02   Median :479.5  
+# Mean   :2014-09-02   Mean   :485.7  
+# 3rd Qu.:2014-10-17   3rd Qu.:591.0  
+# Max.   :2014-12-01   Max.   :656.1 
+
+dt02[dt02$Price == 309.]
+
 
 
 # plot entire Bitcoin lifetime with Agora -------------------------------------
@@ -123,6 +163,9 @@ par(mar = c(6, 6, 6, 6), family = "FranklinGothicSSK")
 plot(bpi$Date, bpi$Price, col = "#00000075",
      main = "Bitcoin Price Index (USD) 2010-07-18 :: 2016-08-02")
 abline(a = 213.50, b = 0, lty = 2, col = "#FF000075")
+abline(a = 300, b = 0, lty = 2, col = "#FF000050")
+abline(a = 350, b = 0, lty = 2, col = "#FF0000")
+abline(a = 400, b = 0, lty = 2, col = "#FF000025")
 points(bpiAg$Date, bpiAg$Price, col = "#CD0000", pch = 19, cex = 0.8)
 points(bpiAg$Date, bpiAg$Price, col = "#00000050", pch = 1, cex = 1.1)
 
