8ac1ea5057ee029b874a08f000c335e3eacbda41
Pete Deevakul <editor@mozzarella.website>
2016-08-29 01:42:18 -0400

    categorical plots

--- R/association-rules-2014-02.R
+++ R/association-rules-2014-02.R
@@ -8,21 +8,25 @@ library(arules)
 library(arulesViz)
 library(data.table)
 library(tm)
-library(dplyr)
 
-p14 <- fread("~/GitHub/agora-data/Agora2014a.csv", stringsAsFactors = T)
+p14 <- fread("~/GitHub/agora-data/ag03-2014.csv", stringsAsFactors = F)
 p14$date <- as.Date(p14$date)
 str(p14)
 
+p14$to <- gsub("wideutschland", "", p14$to)
+p14$to <- gsub("deutschlandstination", "", p14$to)
+p14$to <- (as.factor(p14$to))
 
 # quick looks ---------------------------------------------
 
-length(unique(p14$product)) # 56746
-summary(p14$price)
-#     Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-#       0       0       0      41       1 3521000
+length(unique(p14$product)) # 62873
+summary(p14$usd)
+#           Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
+#         0         29         95      15890        332 2215000000
 
-quantile(p14$price)
+quantile(p14$usd)
+#          0%                  25%                  50%                  75%                 100% 
+# 0.000031244         29.339983719         95.382603045        331.517211912 2214586310.860000134
 
 # look at high price / placeholder listings
 waitlist <- subset(p14, p14$price >= 3500)
@@ -33,17 +37,159 @@ waitTable <- waitTable[order(waitTable$Freq, decreasing = T), ]
 colnames(waitTable) <- c("subCategory", "Freq")
 table(waitlist$cat)
 
+# snapshotp of what's out of stock
 waitT2 <- as.data.frame(table(waitlist$subsubcat))
 
-opioids <- subset(p14, p14$subcat == "Opioids")
-unique(opioids$date)
-opioids$list[opioids$date == "2014-07-29"]
-is.na(opioids$subsubcat)
-levels(opioids$subsubcat)
-
 # subset out the placeholder (high) prices ------------------------------------
-p14 <- subset(p14, p14$price < 3500) # 772357
 
+p2014 <- p14
+p14 <- subset(p14, p14$price < 1200) # 1017556
+
+summary(p14$usd)
+#     Min.  1st Qu.   Median     Mean  3rd Qu.     Max. 
+#      0.0     29.3     95.3    663.8    330.6 748000.0
+
+# even with the hard subset - the right skew remains.
+# plot to see what's going on with outliers
+
+library(ggplot2)
+library(vcd)
+
+# how much --------------------------------------------------------------------
+par(mar = c(7, 11, 8, 7), las = 1,  bty = "l", family = "HersheySans")
+plot(p14$date, p14$usd, ylab = "$", col = "#00000090",
+     main = "Agora Marketplace 2014: Product Prices < $750k")
+
+plot(p14$date, log2(p14$usd), ylab = "log2(usd)", col = "#00000090",
+     main = "Agora Marketplace 2014: Product Prices < $750k (log base 2 USD)")
+
+kde <- density(p14$usd, kernel = "gaussian")
+plot(kde)
+
+# where and what? -------------------------------------------------------------
+par(mar = c(10, 22, 4, 6), las = 2,  bty = "l", family = "HersheySans")
+spineplot(p14$cat, p14$from, main = "plot(p14$cat, p14$from)",
+          xlab = "", ylab = "", subset(p14, p14$usd <= 1000))
+spineplot(p14b$subcat, p14b$from, main = "(p14b$subcat, p14b$from)",
+          xlab = "", ylab = "")
+spineplot(p14b$subsubcat, p14b$from, main = "plot(p14b$subsubcat, p14b$from)",
+          xlab = "", ylab = "")
+
+
+# 1000 USD subset -------------------------------------------------------------
+# curious - at what USD max does the data distribute normally?
+
+# subset list under $1000 with/without feedback.
+p14b <- subset(p14, p14$usd <= 1000) # 905805 
+
+summary(p14b$usd)
+#     Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#    0.00   24.58   75.23  162.00  209.40 1000.00
+
+par(mar = c(7, 11, 8, 7), las = 1,  bty = "l", family = "HersheySans")
+plot(p14b$date, p14b$usd, ylab = "$", col = "#00000090",
+     main = "Agora Marketplace 2014: Product Prices < $1,000")
+
+# Feedbacks ONLY --------------------------------------------------------------
+p14$feedback <- as.character(p14$feedback)
+
+# list under 1000$ USD and contains feedback.
+p14c <- subset(p14,  p14$feedback != " Feedbacks: No feedbacks found. " &
+                 p14$usd <= 1000) # 442716
+
+summary(p14c)
+p14c$from[which(is.na(p14c$from))] <- "no info"
+levels(as.factor(p14c$from))
+
+# safety
+write.csv(p14c, file = "~/GitHub/agora-data/ag04-2014.csv", row.names = F)
+p14 <- fread("~/GitHub/agora-data/ag04-2014.csv", stringsAsFactors = T)
+
+p14$from <- gsub("the united snakes of captivity", "usa", p14$from)
+p14$from <- as.factor(p14$from)
+
+par(mar = c(10, 16, 4, 6), las = 2,  bty = "l", family = "HersheySans")
+spineplot(p14$cat, p14$from, main = "plot(p14c$cat, p14c$from)",
+          xlab = "", ylab = "", ylevels = seq(69, 0, -1))
+
+spineplot(p14$from, p14$cat, main = "plot(p14c$from, p14c$cat)",
+          xlab = "", ylab = "", ylevels = seq(69, 0, -1))
+
+spineplot(p14$subcat, p14$from, main = "plot(p14$subcat, p14$from)",
+          xlab = "", ylab = "", ylevels = seq(69, 0, -1),
+          xaxlabels = sort(levels(p14$subcat), decreasing = T),
+          yaxlabels = sort(levels(p14$from), decreasing = T))
+
+spineplot(p14$from, p14$subcat, main = "plot(p14$from, p14$subcat)",
+          xlab = "", ylab = "", ylevels = seq(69, 0, -3),
+          xaxlabels = sort(levels(p14$from), decreasing = T),
+          yaxlabels = sort(levels(p14$subcat), decreasing = F))
+
+spineplot(p14$subsubcat, p14$from, main = "plot(p14$subsubcat, p14$from)",
+          xlab = "", ylab = "", ylevels = seq(69, 0, -1),
+          xaxlabels = sort(levels(p14$subsubcat), decreasing = T),
+          yaxlabels = sort(levels(p14$from), decreasing = T))
+
+spineplot(p14$from, p14$subsubcat, main = "plot(p14$subsubcat, p14$from)",
+          xlab = "", ylab = "", ylevels = seq(69, 0, -1),
+          yaxlabels = sort(levels(p14$subsubcat), decreasing = T))
+
+levels(p14$subsubcat)
+
+ft <- as.data.frame(table(p14$from))
+colnames(ft) <- c("location", "count")
+
+
+# reduce variables for better mosaic plots
+mosaic(count~location, data = ft)
+mosaic(location~count, data = ft)
+
+
+
+
+# Densities: Price, Categories, Locations, Products ---------------------------
+library(ggplot2)
+library(RColorBrewer)
+library(extrafont)
+library(extrafontdb)
+font_import()
+
+p1 <- ggplot(p14, aes(usd)) + theme_minimal() +
+  geom_density(fill = "#CD0000", alpha = 0.2) +
+  geom_line(stat = "density") + expand_limits(y = 0) +
+  theme(plot.title = element_text(family= "Times", face = "bold", size = 16)) +
+  labs(title = "AgMarket 2014 - List Price Density", x = "list price in USD")
+
+p1 + theme(axis.title.x = element_text(family = "Times", face = "italic", size = 12)) +
+  theme(axis.title.y = element_text(family = "Times", face = "italic", size = 12)) +
+  theme(axis.text.x = element_text(family = "Times", face = "plain", size = 12)) +
+  theme(axis.text.y = element_text(family = "Times", face = "plain", size = 12)) +
+  theme(axis.title.y = element_text(margin = margin(0, 20, 0, 0))) + 
+  theme(axis.title.x = element_text(margin = margin(40, 0, 0, 0))) +
+  theme(plot.margin = unit(c(2, 2, 2, 2), "cm"))
+
+p2 <- ggplot(p14, aes(x = sqrt(usd), fill = usd)) + 
+  geom_density(alpha = 0.5) +
+  geom_histogram(mapping = )
+  xlim(-10, 100)
+
+p2
+
+p3 <- ggplot(p14, aes(subcat, from, fill = cat)) + geom_tile() + 
+  labs(title = "Agora 2014: Category / Subcategory :: Location", 
+       y = "", x = expression(subcat %subset% cat)) +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(-20, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 9))
+  
+p3 + scale_fill_manual(values = c("red3", "#998976", "#B2DFEE", "#9AC0CD", 
+                                  "#B2DFEE", "red2", "bisque2", "gray23",
+                                  "bisque3", "bisque4", "bisque", "bisque4",
+                                  "firebrick4"))
 
 p14$price <- discretize(p14$price, method = "cluster", categories = 12)
 levels(p14$price)
