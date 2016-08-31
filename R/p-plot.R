9416f24d5c2fda9fe2762d4ecaa112a1acf4bcc2
Pete Deevakul <editor@mozzarella.website>
2016-08-31 17:14:01 -0400

    drug - location - price plots

--- R/p-plot.R
+++ R/p-plot.R
@@ -4,101 +4,214 @@
 
 # load data -------------------------------------------------------------------
 
-library(arules)
-library(arulesViz)
 library(data.table)
-library(tm)
 
-p14 <- fread("~/GitHub/agora-data/ag03-2014.csv", stringsAsFactors = F)
+p14 <- fread("~/GitHub/agora-data/ag06-2014.csv", stringsAsFactors = F)
 p14$date <- as.Date(p14$date)
 str(p14)
 
-p14$to <- gsub("wideutschland", "", p14$to)
-p14$to <- gsub("deutschlandstination", "", p14$to)
-p14$to <- (as.factor(p14$to))
-
+length(unique(p14$from)) # 81
 length(unique(p14$product)) # 62873
 summary(p14$usd)
 #    Min.   1st Qu.    Median      Mean   3rd Qu.      Max. 
 # 0         29         95      15890        332 2215000000
-
-
-# subset over $1000 -----------------------------------------------------------
-
-# Placeholders and Outliers
-waitlist <- subset(p14, p14$price >= 3500)
-waitlist <- waitlist[order(waitlist$price, decreasing = T)]
-
-waitTable <- as.data.frame(table(waitlist$subcat))
-waitTable <- waitTable[order(waitTable$Freq, decreasing = T), ]
-colnames(waitTable) <- c("subCategory", "Freq")
-table(waitlist$cat)
-
-# snapshotp of what's out of stock
-waitT2 <- as.data.frame(table(waitlist$subsubcat))
-
-# 1000 USD subset -------------------------------------------------------------
-
-# subset without feedback
-p14b <- subset(p14, p14$usd <= 1000) # 905805 
-
-# subset including feedback
-p14$feedback <- as.character(p14$feedback)
-p14c <- subset(p14,  p14$feedback != " Feedbacks: No feedbacks found. " &
-                 p14$usd <= 1000) # 442716
-
-summary(p14c)
-p14c$from[which(is.na(p14c$from))] <- "no info"
-levels(as.factor(p14c$from))
-
-# safety
-write.csv(p14c, file = "~/GitHub/agora-data/ag04-2014.csv", row.names = F)
-p14 <- fread("~/GitHub/agora-data/ag04-2014.csv", stringsAsFactors = T)
-
-p14$from <- gsub("the united snakes of captivity", "usa", p14$from)
-p14$from <- as.factor(p14$from)
 
 # Densities: Price, Categories, Locations, Products ---------------------------
 
 library(ggplot2)
 library(RColorBrewer)
 library(vcd)
+library(extrafont)
 library(extrafontdb)
 font_import()
 
-p1 <- ggplot(p14, aes(usd)) + theme_minimal() +
-  geom_density(fill = "#CD0000", alpha = 0.2) +
-  geom_line(stat = "density") + expand_limits(y = 0) +
-  theme(plot.title = element_text(family= "Times", face = "bold", size = 16)) +
-  labs(title = "AgMarket 2014 - List Price Density", x = "list price in USD")
+# clean locations (more) ------------------------------------------------------
+p14$from[which(is.na(p14$from))] <- "no info"
+p14$from[p14$from == "null"] <- "no info"
 
-p1 + theme(axis.title.x = element_text(family = "Times", face = "italic", size = 12)) +
-  theme(axis.title.y = element_text(family = "Times", face = "italic", size = 12)) +
-  theme(axis.text.x = element_text(family = "Times", face = "plain", size = 12)) +
-  theme(axis.text.y = element_text(family = "Times", face = "plain", size = 12)) +
-  theme(axis.title.y = element_text(margin = margin(0, 20, 0, 0))) + 
-  theme(axis.title.x = element_text(margin = margin(40, 0, 0, 0))) +
-  theme(plot.margin = unit(c(2, 2, 2, 2), "cm"))
+p14$from <- gsub("^usauk(.*)", "usa", p14$from)
+p14$from <- gsub("us & canada", "usa", p14$from)             
+p14$from <- gsub("usa uk & eu", "usa", p14$from)                            
+p14$from <- gsub("usa, uk & eu", "usa", p14$from)                            
+p14$from <- gsub("usa, uk and worldwide", "usa", p14$from)                   
+p14$from <- gsub("usa& uk", "usa", p14$from)                                 
+p14$from <- gsub("usaand uk", "usa", p14$from)                                
+p14$from <- gsub("west of the mississippi", "usa", p14$from)
+p14$from <- gsub("usa uk", "usa", p14$from)
+p14$from <- gsub("usawide", "usa", p14$from)
 
-p2 <- ggplot(p14, aes(x = sqrt(usd), fill = usd)) + 
-  geom_density(alpha = 0.5) +
-  geom_histogram(mapping = )
-xlim(-10, 100)
+p14$from <- gsub("uk and ireland", "uk", p14$from)
+p14$from <- gsub("uk, usa philippines", "uk", p14$from)
+p14$from <- gsub("uk, usa& asia", "uk", p14$from) 
+p14$from <- gsub("uk, usa& worldwide", "uk", p14$from)
+p14$from <- gsub("uk , usa& worldwide", "uk", p14$from)
+p14$from <- gsub("uk\\s,\\suk", "uk", p14$from)
+p14$from <- gsub("uk, usaand worldwide", "uk", p14$from)
+p14$from <- gsub("uk , usaand worldwide", "uk", p14$from)
+p14$from <- gsub("uk, usa eu, aus", "uk", p14$from)
+p14$from <- gsub("ukwide", "uk", p14$from)
 
-p2
+p14$from <- gsub("china or eu", "china", p14$from)
 
+p14$from <- gsub("the loins of our founding fathers", "usa", p14$from)
+p14$from <- gsub("george washingtons boner holder", "usa", p14$from)
+p14$from <- gsub("the home of the body bags, shotty, and mag", "usa", p14$from)
+p14$from <- gsub("the home of the body bags", "usa", p14$from)
+p14$from <- gsub("pacific palisades", "usa", p14$from)
+p14$from <- gsub("pacific palisades", "usa", p14$from)
+p14$from <- gsub("la jolla", "usa", p14$from)
+p14$from <- gsub("the united snakes of captivity", "usa", p14$from)
+
+p14$from <- gsub("rusa federation", "russian federation", p14$from)
+
+p14$from <- gsub("worldwidewide", "worldwide", p14$from)
+p14$from <- sub("the home of the body bags, shotty, and mag, shotty, and mag", 
+                 "the home of the body bags, shotty, and mag", p14$from)
+p14$from <- gsub("shipping", "no info", p14$from)
+p14$from <- gsub("worldwidewide", "worldwide", p14$from)
+p14$from <- gsub("worldwidewide", "worldwide", p14$from)
+
+p14$subcat[p14$subcat == "Ecstasy-MDMA"] <- "Ecstasy"
+p14$subcat[p14$subcat == "Ecstasy-NoSub"] <- "Ecstasy"
+
+write.csv(p14, file = "~/GitHub/agora-data/ag06-2014.csv", row.names = F)
+
+# flawed - complete plot
 p3 <- ggplot(p14, aes(subcat, from, fill = cat)) + geom_tile() + 
   labs(title = "Agora 2014: Category / Subcategory :: Location", 
-       y = "", x = expression(subcat %subset% cat)) +
+       y = "", x = expression(subcat %subset% cat), fill = "category") +
   theme(plot.title = element_text(family= "FranklinGothicSSK", 
                                   face = "bold", size = 14,
-                                  margin = margin(-20, 0, 0, 0))) + 
+                                  margin = margin(0, 0, 0, 0))) + 
   theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
   theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
                                    angle = 45, hjust = 1)) +
   theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 9))
 
-p3 + scale_fill_manual(values = c("red3", "#998976", "#B2DFEE", "#9AC0CD", 
-                                  "#B2DFEE", "red2", "bisque2", "gray23",
-                                  "bisque3", "bisque4", "bisque", "bisque4",
+p3 + scale_fill_manual(values = c("red3", "#CDAD00", "#00688B", "#9AC0CD", 
+                                  "#FF7F00", "red2", "bisque3", "gray23",
+                                  "bisque2", "bisque1", "darkorange3", "darkorange4",
                                   "firebrick4"))
+
+# remove eletronics and jewelry
+p14b <- subset(p14, p14$cat != "Electronics" & p14$cat != "Jewelry") # 10000591
+
+p3b <- ggplot(p14b, aes(subcat, from, fill = cat)) + geom_tile() + 
+  labs(title = "Agora 2014: Category / Subcategory :: Location", 
+       y = "", x = "", fill = "category") +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(0, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 9))
+
+p3b + scale_fill_manual(values = c("red3", 
+                                  "bisque4", 
+                                  "#00688B", 
+                                  "#9AC0CD", 
+                                  "red1", 
+                                  "bisque1",
+                                  "black", 
+                                  "bisque3", 
+                                  "darkorange2", 
+                                  "darkorange4",
+                                  "firebrick4"))
+
+
+# subset under 1k -------------------------------------------------------------
+
+p4 <- subset(p14, p14$usd <= 1000) # 905805
+
+p4p <- ggplot(p4, aes(subsubcat, from, fill = usd)) + geom_tile() +
+  labs(title = "Agora 2014: Drug ~ Location / Price (USD < 1,000)", 
+       y = "", x = "", fill = "Price (USD)") +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(0, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 9))
+
+p4p + scale_fill_gradient2(low = "deepskyblue4", mid = "bisque1", high = "firebrick3",
+                           midpoint = 500)
+
+# subset over 1k  -------------------------------------------------------------
+
+p5 <- subset(p14, p14$usd > 1000 & p14$usd < 10000.000) # 11069
+
+p5p <- ggplot(p5, aes(subsubcat, from, fill = usd)) + geom_tile() +
+  labs(title = "Agora 2014: Drug ~ Location / Price (10,000 > USD > 1,000)", 
+       y = "", x = "", fill = "Price (USD)") +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(0, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 9))
+
+p5p + scale_fill_gradient2(low = "deepskyblue4", mid = "bisque1", high = "firebrick3",
+                           midpoint = 5000)
+
+
+# subset over 10-40k  -------------------------------------------------------------
+
+p6 <- subset(p14, p14$usd > 10000 & p14$usd < 40000.000) # 4722
+
+p6p <- ggplot(p6, aes(subsubcat, from, fill = usd)) + geom_tile() +
+  labs(title = "Agora 2014: Drug ~ Location / Price (40,000 > USD > 10,000)", 
+       y = "", x = "", fill = "Price (USD)") +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(0, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 10.5))
+
+p6p + scale_fill_gradient2(low = "deepskyblue4", mid = "bisque1", high = "firebrick3",
+                           midpoint = 25000)
+
+# subset over 40k  ----------------------------------------------------------------
+
+p7 <- subset(p14, p14$usd > 40000 & p14$usd < 1000000) # 1416
+
+p7p <- ggplot(p7, aes(subsubcat, from, fill = usd)) + geom_tile() +
+  labs(title = "Agora 2014: Drug ~ Location / Price (1,000,000 > USD > 40,000)", 
+       y = "", x = "", fill = "Price (USD)") +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(0, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 10.5))
+
+p7p + scale_fill_gradient2(low = "deepskyblue4", mid = "bisque1", high = "firebrick3",
+                           midpoint = 520000)
+
+# subset over 1m  ------------------------------------------------------------------
+
+p8 <- subset(p14, p14$usd > 1000000 & p14$usd < 10020993 & 
+               p14$vendor != "DutchTruthTeller") # 187
+
+summary(p8$usd)
+range(p8$usd)
+
+p8p <- ggplot(p8, aes(subsubcat, from, fill = usd)) + geom_tile() +
+  labs(title = "Agora 2014: Drug ~ Location / Price (USD > 1,000,000)", 
+       y = "", x = "", fill = "Price (USD)") +
+  theme(plot.title = element_text(family= "FranklinGothicSSK", 
+                                  face = "bold", size = 14,
+                                  margin = margin(0, 0, 0, 0))) + 
+  theme(plot.margin = unit(c(2, 2, 1, 2.5), "cm")) +
+  theme(axis.text.x = element_text(family = "FranklinGothicSSK", size = 10.5,
+                                   angle = 45, hjust = 1)) +
+  theme(axis.text.y = element_text(family = "FranklinGothicSSK", size = 10.5))
+
+p8p + scale_fill_gradient2(low = "deepskyblue4", mid = "bisque1", high = "firebrick3",
+                           midpoint = 4960000)
+
