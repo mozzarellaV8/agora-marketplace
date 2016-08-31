000d70f9d6b328c3cc13ff5852d31e4f618e3f20
Pete Deevakul <editor@mozzarella.website>
2016-08-31 11:35:02 -0400

    frequency tables across variables

--- R/tables.R
+++ R/tables.R
@@ -8,7 +8,7 @@ library(arulesViz)
 library(data.table)
 library(tm)
 
-p14 <- fread("~/GitHub/agora-data/ag03-2014.csv", stringsAsFactors = T)
+p14 <- fread("~/GitHub/agora-data/ag05-2014.csv", stringsAsFactors = T)
 p14$date <- as.Date(p14$date)
 str(p14)
 
@@ -18,21 +18,25 @@ str(p14)
 tDate <- as.data.frame(table(p14$date))
 colnames(tDate) <- c("date", "freq")
 
-# who?
+# who? most prolific vendors
 tv <- as.data.frame(table(p14$vendor))
 
-# what?
+# what? most frequently listed products
 tp <- as.data.frame(table(p14$product))
 
 # how much?
-tPri <- as.data.frame(table(p14$price))
-tu <- as.data.frame(table(p14$usd))
+tBTC <- as.data.frame(table(p14$price))
+tUSD <- as.data.frame(table(p14$usd))
 
 # where from?
-tf <- as.data.frame(table(p14$from))
+tFrom <- as.data.frame(table(p14$from))
 
 # what category?
 tc <- as.data.frame(table(p14$cat))
 tsc <- as.data.frame(table(p14$subcat))
 tssc <- as.data.frame(table(p14$subsubcat))
 
+# cross tablulation
+cl <- xtabs(~cat + from + subsubcat, data = p14)
+print(cl)
+summary(cl)
